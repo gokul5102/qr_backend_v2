1a8d9060b1396b9ea7437e44a3113daf67ec94a4
Gokul <gokulr5102@gmail.com>
2022-09-18 11:31:45 +0530

    Prod Db

--- qr_backend/settings.py
+++ qr_backend/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'qr_backend.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # prod
